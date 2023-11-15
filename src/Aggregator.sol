e847dcda88f3ee4eebc2d027db6fbe977bc0047e
Aaron Zachariah <aaron.zachariah@gmail.com>
2023-11-15 18:02:06 -0500

    updated logic for 2D structures

--- src/Aggregator.sol
+++ src/Aggregator.sol
@@ -7,29 +7,34 @@ contract FederatedAggregator {
     // contains an addr and a clients individual model params
     struct Client {
         address clientAddress;
-        uint256[] modelParameters;
+        uint256 maxIndex;
     }
+
+    // mapping(uint256 => uint256[]) represents the 2D array for each client
+    // each client will have a client id, which is mapped to the 2D array mapping
+    mapping(address => mapping(uint256 => uint256[])) clientParameters;
+    
 
     // Struct to represent the global model (server)
-    // contains num_clients and the model params
+    // maxIndex used for the mapping
     struct Server {
-        uint256[] aggregatedParameters;
         uint256 clientCount;
+        uint256 maxIndex;
     }
-    
+    // server params as a state variable (mapping can only exist in storage)
+    mapping(uint256 => uint256[]) aggregatedParameters;
+
     Client[] clients;
     Server public server;
-    
-    // keep track of amount of times aggregation has been called
+
     uint256 aggregationRounds = 0;
 
-    // Maximum Client count
     uint256 MAX_CLIENTS = 4;
 
     // initialise server in constructor
     constructor() {
         owner = msg.sender;
-        server = Server({aggregatedParameters: new uint256[](10), clientCount: 0}); // 10 output labels for MNIST
+        server = Server({clientCount: 0, maxIndex: 0});
     }
 
     // function to create a client object and add it to the list
@@ -37,42 +42,46 @@ contract FederatedAggregator {
         require(msg.sender != address(0), "Invalid client address");
         require(aggregationRounds == 0, "Communication Rounds have started. Cannot add new clients.");
         require(clients.length <= MAX_CLIENTS, "Max Client Count reached. Cannot add any more clients.");
-        clients.push(Client({clientAddress: msg.sender, modelParameters: new uint256[](10)}));
+ 
+        clients.push(Client({clientAddress: msg.sender, maxIndex: 0}));
     }
 
-    function getAggregatedWeights()  view public returns (uint256[] memory) {
-        return server.aggregatedParameters;
+
+    function getAggregatedWeights()  view public returns (uint256[][] memory) {
+        uint256[][] memory params = new uint256[][](server.maxIndex);
+        for(uint i = 0 ;i < server.maxIndex; i++ ) {
+            params[i] = aggregatedParameters[i];
+        }
+        return params;
     }
 
     // function to update params for a client
     // if the message sender is a client, will update params with the provided arg
-    function updateParticipantParameters(uint256[] memory newParameters) public {
-        // check for correct length
-        require(newParameters.length == server.aggregatedParameters.length, "Invalid parameter length");
-        for (uint256 i = 0; i < clients.length; i++) {
+    function updateParticipantParameters(uint256[][] memory newParameters) public {
+        // search for client and update maxIndex
+        bool found = false;
+        for (uint i = 0; i < clients.length; i++){
             if (clients[i].clientAddress == msg.sender) {
-                clients[i].modelParameters = newParameters;
-                return;
+                clients[i].maxIndex = newParameters.length;
+                found = true;
             }
         }
-        // client addr not found
-        revert("Participant not found");
-    }
+        if (found == false){
+            revert("Sender is not a participant");
+        }
 
+        // update params
+        for (uint i = 0;i < newParameters.length; i++){
+            clientParameters[msg.sender][i] = newParameters[i];
+        }   
+    }
+    
     // functiont to aggregate the client params into the global params using Federated Average
     function aggregate() public {
-        require(clients.length > 0, "No clients available");
-        require(msg.sender == owner, "Only the owner can start aggregation");
-        // perform averaging over the 10 output labels
-        for (uint256 i = 0; i < server.aggregatedParameters.length; i++){
-            // get the sum for each client
-            uint256 sum = 0;
-            for (uint256 j = 0; j < clients.length; j++) {
-                sum += clients[j].modelParameters[i];
-            }
-            // compute and store the avg
-            server.aggregatedParameters[i] = sum / clients.length;
-        }
+        // require(clients.length > 0, "No clients available");
+        // require(msg.sender == owner, "Only the owner can start aggregation");
+        // TODO: implement new algorithm
+        
     }
 
 
