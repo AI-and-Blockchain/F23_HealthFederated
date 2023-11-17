c5b7f5216bf5561e3bfc4b6ad92825ff589ac26e
linhhtran <98370798+linhhtran@users.noreply.github.com>
2023-11-17 12:29:51 -0500

    Add files via upload

--- src/vfl_alg.py
+++ src/vfl_alg.py
@@ -11,68 +11,7 @@ from torch.optim.lr_scheduler import ExponentialLR
 from torch.autograd import Variable
 from torch.distributions.binomial import Binomial
 
-class ClientModel(nn.Module):
-    def __init__(self):
-        super(ClientModel, self).__init__()
-        self.densenet = torchvision.models.densenet169(pretrained=True)
-        self.resnet = torchvision.models.resnet50(pretrained=True)
-        self.vgg = torchvision.models.vgg19(pretrained=True)
-
-        # Remove the classification layers (fully connected layers)
-        self.densenet = nn.Sequential(*list(self.densenet.children())[:-1])
-        self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
-        self.vgg = nn.Sequential(*list(self.vgg.children())[:-1])
-
-    def forward(self, x):
-        x1 = self.densenet(x)
-        x2 = self.resnet(x)
-        x3 = self.vgg(x)
-
-        # Flatten and concatenate
-        x1 = x1.view(x1.size(0), -1)
-        x2 = x2.view(x2.size(0), -1)
-        x3 = x3.view(x3.size(0), -1)
-        x = torch.cat((x1, x2, x3), dim=1)
-
-        return x
-
-class ServerModel(nn.Module):
-    def __init__(self):
-        super(ServerModel, self).__init__()
-        self.densenet = torchvision.models.densenet169(pretrained=True)
-        self.resnet = torchvision.models.resnet50(pretrained=True)
-        self.vgg = torchvision.models.vgg19(pretrained=True)
-
-        # Remove the classification layers (fully connected layers)
-        self.densenet = nn.Sequential(*list(self.densenet.children())[:-1])
-        self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
-        self.vgg = nn.Sequential(*list(self.vgg.children())[:-1])
-
-        # Define new classification layers
-        self.fc = nn.Sequential(
-            nn.Dropout(0.4),
-            nn.BatchNorm1d(53760), # Output features from densenet + resnet + vgg
-            nn.Linear(53760, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(128, 2),
-        )
-
-    def forward(self, x):
-        x1 = self.densenet(x)
-        x2 = self.resnet(x)
-        x3 = self.vgg(x)
-
-        # Flatten and concatenate
-        x1 = x1.view(x1.size(0), -1)
-        x2 = x2.view(x2.size(0), -1)
-        x3 = x3.view(x3.size(0), -1)
-        x = torch.cat((x1, x2, x3), dim=1)
-
-        x = self.fc(x)
-        return x
+from models import ClientModel, ServerModel
 
 # Arguments and parameters
 num_clients = 4
@@ -89,10 +28,11 @@ optimizers = []
 schedulers = []
 
 for i in range(num_clients+1):
+    model = None
     if i == num_clients:
-        model = ClientModel()
-    else:
         model = ServerModel()
+    else:
+        model = ClientModel()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     scheduler = ExponentialLR(optimizer, gamma=lr_decay)
     
@@ -128,17 +68,80 @@ val_loaders = []
 test_loaders =[]
 
 for i in range(num_clients):
-    train_data = torchvision.datasets.ImageFolder(root=f'C:/Users/huyfr/Documents/AI&Blockchain/project/SplitCovid19/client{i}', transform=transform)
+    train_data = torchvision.datasets.ImageFolder(root=f'/gpfs/u/home/VFLA/VFLAnrnl/scratch/SplitCovid19/client{i}', transform=transform)
     train_data = torch.utils.data.Subset(train_data, range(10))
     val_data = torch.utils.data.Subset(train_data, range(10, 20))
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=False,  num_workers=4)
     train_loaders.append(train_loader)
     val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False,  num_workers=4)
     val_loaders.append(val_loader)
-    test_data = torchvision.datasets.ImageFolder(root=f'C:/Users/huyfr/Documents/AI&Blockchain/project/SplitCovid19/client{i}', transform=transform)
+    test_data = torchvision.datasets.ImageFolder(root=f'/gpfs/u/home/VFLA/VFLAnrnl/scratch/SplitCovid19/client{i}', transform=transform)
     test_data = torch.utils.data.Subset(test_data, range(10))
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=4)
     test_loaders.append(test_loader)
+
+# train function
+def train_with_blockchain():
+    
+    targets = None
+    embeddings_grad = [None] * num_clients
+    embeddings_nograd = [None] * num_clients
+    
+    completed = False
+    train_iterators = []
+    for i in range(num_clients):
+        train_iterators.append(iter(train_loaders[i]))
+    
+    while not completed:
+        # At party side
+        for i in range(num_clients):
+            # get current batch
+            item = next(train_iterators[i], -1)
+            
+            if item == -1:
+                completed = True
+                break
+            
+            inputs, targets = item
+
+            # generate embedding
+            embeddings_grad[i] = models[i](inputs)
+            with torch.no_grad():
+                embeddings_nograd[i] = models[i](inputs)
+
+            # add differential privacy noise
+            embeddings_nograd[i] = quantize(embeddings_nograd[i], theta, quant_bin)
+
+            # send embeddings (embeddings_nograd[i]) to smart contract
+            ???
+        
+        if completed:
+            break
+
+        # At server side
+        # retrieve the embedding sum from smart contract
+        with torch.no_grad():
+            sum_nograd = ???
+
+        # dequantize the discrete sum into continuous sum
+        sum_nograd = dequantize(sum_nograd, theta, quant_bin, num_clients)
+        sum_grad = torch.sum(torch.stack(embeddings_grad),axis=0)
+        sum_grad.data = sum_nograd
+
+        # compute outputs
+        outputs = models[num_clients](sum_grad)
+        loss = criterion(outputs, targets)
+
+        # parties and server compute gradient and do SGD step
+        for i in range(num_clients + 1):
+            optimizers[i].zero_grad()
+        loss.backward()
+        for i in range(num_clients + 1):
+            optimizer[i].step()
+    
+        # parties and server calculate new learning rate
+        for i in range(num_clients + 1):
+            schedulers[i].step()
 
 # train without blockchain
 def train_without_blockchain():
@@ -241,7 +244,7 @@ def evaluate(mode):
 
         # compute outputs
         outputs = models[num_clients](embedding_sum)
-        loss = criterion(outputs, targets)
+        loss = criterion(outputs, torch.nn.functional.one_hot(targets, num_classes=2).float())
         _, predicted = torch.max(outputs.data, 1)
 
         # compute accuracy
@@ -258,16 +261,16 @@ def evaluate(mode):
     return (accuracy, loss)
 
 # initial loss
-test_accuracy, test_loss = evaluate(mode = 'test')
-print('Initial test loss: {:.2f} \t Initial test accuracy: {:.2f}'.format(test_loss, test_accuracy))
+#test_accuracy, test_loss = evaluate(mode = 'test')
+#print('Initial test loss: {:.2f} \t Initial test accuracy: {:.2f}'.format(test_loss, test_accuracy))
 
 # main training loop
 for epoch in range(num_epochs):
     print('\n-----------------------------------')
     print('Epoch: [%d/%d]' % (epoch+1, num_epochs))
 
-    train_without_blockchain()
-    val_accuracy, val_loss = evaluate(mode = 'validation')
-    test_accuracy, test_loss = evaluate(mode = 'test')
+    train_with_blockchain()
+    #val_accuracy, val_loss = evaluate(mode = 'validation')
+    #test_accuracy, test_loss = evaluate(mode = 'test')
     
-    print('Val Loss: {:.2f} \t Val Accuracy: {:.2f} \t Test Loss: {:.2f} \t Test Accuracy: {:.2f}'.format(val_loss, val_accuracy, test_loss, test_accuracy))
+    #print('Val Loss: {:.2f} \t Val Accuracy: {:.2f} \t Test Loss: {:.2f} \t Test Accuracy: {:.2f}'.format(val_loss, val_accuracy, test_loss, test_accuracy))
